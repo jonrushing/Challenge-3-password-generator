45ba47faa59725375072738ee23fa1cd8e869af9
Jon <Jon@Minettes-MacBook-Pro.local>
2022-09-18 16:06:09 -0400

    added prompt and alerts for password character length

--- script.js
+++ script.js
@@ -4,6 +4,29 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+
+  var inputLength = window.prompt("How long would you like your password? (Between 8-128 characters)")
+  
+  var passwordLength = parseInt(inputLength)
+
+  var inputAlert = "Please choose a number between 8-128"
+
+    if (isNaN (passwordLength)) {
+      window.alert(inputAlert)
+      return
+        
+    }
+
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert(inputAlert)
+      return
+    }
+    
+}
+
+console.log(generatePassword)
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
